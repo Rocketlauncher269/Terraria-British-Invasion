3f771fa4925cd4e10ee1ca010d2926dacdb2ac7a
clyder8 <109995796+clyder8@users.noreply.github.com>
2025-03-31 16:02:56 -0400

    Improved redcoat

--- Content/NPCs/Enemies/RedcoatMusketeer.cs
+++ Content/NPCs/Enemies/RedcoatMusketeer.cs
@@ -9,7 +9,7 @@ using BritishInvasion.Common.Utils;
 
 namespace BritishInvasion.Content.NPCs.Enemies
 {
-    public class Redcoat : ModNPC
+    public class RedcoatMusketeer : ModNPC
     {
         public override void SetStaticDefaults()
         {
@@ -35,56 +35,61 @@ namespace BritishInvasion.Content.NPCs.Enemies
         }
        
         bool Walking=true;
-        bool NeedToLoad=true;
-        int loadTimer=0;
         int shootTimer=0;
-        bool IsShooting=false;
+        int WalkTimer=0;
         public override void AI()
         {
             NPC.TargetClosest(true);
             Player player = Main.player[NPC.target];
-            if(Vector2.Distance(player.Center,NPC.Center)>400f){
+            if(Walking||WalkTimer<60){
                 Utility.AIFighter(NPC, ref NPC.ai, player.Center, accelerationFactor: 0.08f, velMax: 2f, maxJumpTilesX: 3, maxJumpTilesY: 4);
                 Walking=true;
                 shootTimer=0;
-                NeedToLoad=true;
-                loadTimer=0;
-                IsShooting=false;
+                if(Vector2.Distance(player.Center,NPC.Center)<600f)
+                {
+                    WalkTimer++;
+                }
+                if(WalkTimer==60)
+                {
+                    Walking=false;
+                }
             }
             else
             {
                 int frameHeight=58;
                 Walking=false;
                 NPC.velocity.X*=0.9f;
-
-                if(NeedToLoad==true)
-                    NPC.frame.Y = frameHeight *16;
-                loadTimer++;
-                NeedToLoad=false;
-                if(loadTimer==10)
+                
+                shootTimer++;
+                if(player.Center.X !=NPC.Center.X)
                 {
-                    NPC.frame.Y +=  frameHeight;
-                    IsShooting=true;
+                    float slope = (NPC.Center.Y-player.Center.Y)/(Math.Abs(NPC.Center.X-player.Center.X));
+                    NPC.frame.Y = frameHeight * 19;
+                    if(slope > -2.5)
+                        NPC.frame.Y = frameHeight * 18;
+                    if(slope > -0.6)
+                        NPC.frame.Y = frameHeight * 17;
+                    if(slope > 0.6)
+                        NPC.frame.Y = frameHeight * 16;
+                    if(slope > 2.5)
+                        NPC.frame.Y = frameHeight * 15;
+                    
+                    
+
                 }
                 
-                if(IsShooting)
-                {
-                    shootTimer++;
-                    if(shootTimer%15==0)
-                        NPC.frame.Y += frameHeight;
-                    if(NPC.frame.Y==frameHeight*20){
-                        NPC.frame.Y=frameHeight*17;
-                    }
-                    if(NPC.frame.Y==frameHeight*17&&shootTimer%15==10)
-                        Projectile.NewProjectileDirect(NPC.GetSource_FromAI(), NPC.Center+new Vector2(0,-8), (player.Center-NPC.Center).SafeNormalize(Vector2.UnitX)*12f, 180, (int)(NPC.damage/2), 0f, Main.myPlayer);
-                }                       
-
+                if(shootTimer>90){
+                    Projectile.NewProjectileDirect(NPC.GetSource_FromAI(), NPC.Center+new Vector2(0,-8), (player.Center-NPC.Center).SafeNormalize(Vector2.UnitX)*12f, 180, (int)(NPC.damage/2), 0f, Main.myPlayer);
+                    WalkTimer=0;
+                    Walking=true;
+                }
+                                  
             }
             NPC.spriteDirection = -NPC.direction;
         }
 
         private readonly Range walkingFrames = 0..13;
-        private readonly int airFrame = 15;
+        private readonly int airFrame = 14;
         private readonly Range shootingFrames = 15..20;
 
         public override void FindFrame(int frameHeight)
@@ -130,5 +135,5 @@ namespace BritishInvasion.Content.NPCs.Enemies
         {
         }
     }
-    public class RedcoatAlt1 : Redcoat{}
+    public class RedcoatMusketeerAlt1 : RedcoatMusketeer{}
 }
